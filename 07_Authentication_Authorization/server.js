77f3ef17b11cb734f911dd67edfc91cd66dbc96e
Akask-Rai-27 <spyeye118@gmail.com>
2024-03-05 06:17:48 +0530

    authentication

---
+++ 07_Authentication_Authorization/server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express();
+
+const db = require('./db');
+require('dotenv').config();
+
+const bcrypt = require('bcrypt'); 
+
+// passport and passport-local
+const passport = require('./auth')
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); //req.body 
+
+const PORT = process.env.PORT || 3000;
+
+
+
+// Middleware Function 
+const logRequest = (req, res, next) =>{
+    console.log(`[${new Date().toLocaleString()}] Request Made to`);
+    next(); // Move on to the next phase
+}
+
+app.use(logRequest);
+
+
+//to authenticate
+app.use(passport.initialize());
+
+
+// authenticating this / 
+const localAuthMiddleware = passport.authenticate('local',{session:false})
+
+
+app.get('/' ,function(req,res){
+    res.send("welcome to batcave")
+})
+
+
+
+// Import the router files
+const personRoutes = require('./routes/personRoutes');
+
+app.use('/person',localAuthMiddleware,personRoutes);
+
+const menuRoutes = require('./routes/menuRoutes');
+
+app.use('/menu',localAuthMiddleware ,menuRoutes );
+
+
+
+app.listen(PORT, ()=>{
+    console.log('listening on port 3000');
+})
+
